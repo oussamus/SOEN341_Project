6468cfe7b08aade2009cf0a640c4c23c5d27ada2
Deniz Akcal <denizakcal@mail.com>
2019-09-15 20:17:04 -0400

    I committed PHP code that I have confirmed successfully makes use of Postgresql database.

--- src/index.php
+++ src/index.php
@@ -7,7 +7,41 @@
     </head>
     <body>
         <?php
-            echo "Hello yet again world!";
+        
+        // Connecting, selecting database
+        $theHost = "localhost";
+        $theDbName = "soen341db";
+        $theUsername = "deniz"; // "soen341user";
+        $thePassword = "cncrd";
+        
+        $dbconn = pg_connect("host=$theHost dbname=$theDbName user=$theUsername password=$thePassword")
+                or die('Could not connect: ' . pg_last_error());
+
+        // Performing SQL query
+        $nameOfTable = "masa";
+        $query = "SELECT * FROM $nameOfTable;";
+        $result = pg_query($query) or die('Query failed: ' . pg_last_error());
+
+        // Printing results in HTML
+        printTable($result);
+        
+        function printTable($resultOfSqlQuery) {
+            echo "<table border=1>\n"; // Later, make the border stuff be done in CSS (since that's the proper way to do it).
+            while ($line = pg_fetch_array($resultOfSqlQuery, null, PGSQL_ASSOC)) {
+                echo "\t<tr>\n";
+                foreach ($line as $col_value) {
+                    echo "\t\t<td>$col_value</td>\n";
+                }
+                echo "\t</tr>\n";
+            }
+            echo "</table>\n";
+        }
+
+        // Free resultset
+        pg_free_result($result);
+
+        // Closing connection
+        pg_close($dbconn);
         ?>
     </body>
 </html>
